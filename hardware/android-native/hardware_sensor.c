80f729162bc7f490e17ad3e50d44099110ae7de8
i <isomorphisms@sdf.org>
2026-09-22 03:03:21 -0400

    hardware: consume the shared Android accelerometer source

--- hardware/android-native/hardware_sensor.c
+++ hardware/android-native/hardware_sensor.c
@@ -1,5 +1,6 @@
+#include "android_accelerometer.h"
+
 #include <android/looper.h>
-#include <android/sensor.h>
 
 #include <inttypes.h>
 #include <signal.h>
@@ -21,12 +22,14 @@ static const int sensor_looper_id = 1;
 
 static volatile sig_atomic_t stop_requested = 0;
 
-static void request_stop(int signal_number) {
-    (void) signal_number;
+static void request_stop(int signal_number)
+{
+    (void)signal_number;
     stop_requested = 1;
 }
 
-static void print_usage(const char *program) {
+static void print_usage(const char *program)
+{
     fprintf(stderr,
             "usage: %s read PATH [event_count]\n"
             "\n"
@@ -42,7 +45,8 @@ static void print_usage(const char *program) {
             accelerometer_control_path);
 }
 
-static int parse_event_count(const char *text, long *event_count) {
+static int parse_event_count(const char *text, long *event_count)
+{
     char *end = NULL;
     long value = strtol(text, &end, 10);
 
@@ -54,21 +58,19 @@ static int parse_event_count(const char *text, long *event_count) {
     return 0;
 }
 
-static const ASensor *find_accelerometer(ASensorManager *manager) {
-    return ASensorManager_getDefaultSensor(manager, ASENSOR_TYPE_ACCELEROMETER);
-}
-
-static int write_info(const ASensor *sensor) {
-    printf("name\t%s\n", ASensor_getName(sensor));
-    printf("vendor\t%s\n", ASensor_getVendor(sensor));
-    printf("type\t%d\n", ASensor_getType(sensor));
+static int write_info(const struct android_accelerometer *source)
+{
+    printf("name\t%s\n", android_accelerometer_name(source));
+    printf("vendor\t%s\n", android_accelerometer_vendor(source));
+    printf("type\t%d\n", android_accelerometer_type(source));
     printf("units\tm/s^2\n");
-    printf("resolution\t%.9g\n", ASensor_getResolution(sensor));
-    printf("minimum_delay_us\t%d\n", ASensor_getMinDelay(sensor));
+    printf("resolution\t%.9g\n", android_accelerometer_resolution(source));
+    printf("minimum_delay_us\t%d\n", android_accelerometer_minimum_delay_us(source));
     return 0;
 }
 
-static int configure_signal_handlers(void) {
+static int configure_signal_handlers(void)
+{
     struct sigaction action;
 
     memset(&action, 0, sizeof(action));
@@ -86,43 +88,20 @@ static int configure_signal_handlers(void) {
     return 0;
 }
 
-static int stream_accelerometer(ASensorManager *manager,
-                                const ASensor *sensor,
-                                int include_timestamp,
-                                long event_limit) {
-    const int default_period_us = 20000;
-    int period_us = default_period_us;
-    int minimum_delay_us = ASensor_getMinDelay(sensor);
+static int stream_accelerometer(
+    struct android_accelerometer *source,
+    int include_timestamp,
+    long event_limit)
+{
+    const int requested_period_us = 20000;
     long emitted = 0;
-    ALooper *looper = NULL;
-    ASensorEventQueue *queue = NULL;
     int result = 1;
 
-    if (minimum_delay_us > period_us) {
-        period_us = minimum_delay_us;
-    }
-
-    looper = ALooper_prepare(ALOOPER_PREPARE_ALLOW_NON_CALLBACKS);
-    if (looper == NULL) {
-        fprintf(stderr, "could not create Android looper\n");
+    enum android_accelerometer_status enable_status =
+        android_accelerometer_enable(source, requested_period_us);
+    if (enable_status != ANDROID_ACCELEROMETER_OK) {
+        fprintf(stderr, "%s\n", android_accelerometer_status_text(enable_status));
         return 1;
-    }
-
-    queue = ASensorManager_createEventQueue(
-        manager, looper, sensor_looper_id, NULL, NULL);
-    if (queue == NULL) {
-        fprintf(stderr, "could not create accelerometer event queue\n");
-        return 1;
-    }
-
-    if (ASensorEventQueue_enableSensor(queue, sensor) < 0) {
-        fprintf(stderr, "could not enable accelerometer\n");
-        goto cleanup;
-    }
-
-    if (ASensorEventQueue_setEventRate(queue, sensor, period_us) < 0) {
-        fprintf(stderr, "could not set accelerometer event rate\n");
-        goto cleanup;
     }
 
     if (configure_signal_handlers() != 0) {
@@ -133,36 +112,34 @@ static int stream_accelerometer(ASensorManager *manager,
         int poll_result = ALooper_pollOnce(-1, NULL, NULL, NULL);
 
         if (poll_result == ALOOPER_POLL_ERROR) {
-            fprintf(stderr, "Android looper reported an error\n");
+            fprintf(stderr, "Android event loop reported an error\n");
             goto cleanup;
         }
 
         for (;;) {
-            ASensorEvent event;
-            ssize_t received = ASensorEventQueue_getEvents(queue, &event, 1);
+            struct android_accelerometer_reading reading;
+            enum android_accelerometer_read_result read_result =
+                android_accelerometer_next(source, &reading);
 
-            if (received < 0) {
-                fprintf(stderr, "could not read accelerometer event\n");
+            if (read_result == ANDROID_ACCELEROMETER_READ_ERROR) {
+                fprintf(stderr, "could not read an Android accelerometer event\n");
                 goto cleanup;
             }
-            if (received == 0) {
+            if (read_result == ANDROID_ACCELEROMETER_READ_EMPTY) {
                 break;
-            }
-            if (event.type != ASENSOR_TYPE_ACCELEROMETER) {
-                continue;
             }
 
             if (include_timestamp) {
                 printf("%" PRId64 " %.9g %.9g %.9g\n",
-                       event.timestamp,
-                       event.acceleration.x,
-                       event.acceleration.y,
-                       event.acceleration.z);
+                       reading.timestamp_ns,
+                       reading.x,
+                       reading.y,
+                       reading.z);
             } else {
                 printf("%.9g %.9g %.9g\n",
-                       event.acceleration.x,
-                       event.acceleration.y,
-                       event.acceleration.z);
+                       reading.x,
+                       reading.y,
+                       reading.z);
             }
             fflush(stdout);
             emitted += 1;
@@ -176,15 +153,13 @@ static int stream_accelerometer(ASensorManager *manager,
     result = 0;
 
 cleanup:
-    ASensorEventQueue_disableSensor(queue, sensor);
-    ASensorManager_destroyEventQueue(manager, queue);
+    android_accelerometer_disable(source);
     return result;
 }
 
-int main(int argc, char **argv) {
+int main(int argc, char **argv)
+{
     const char *path;
-    ASensorManager *manager;
-    const ASensor *sensor;
     long event_count = 0;
 
     if (argc < 3 || strcmp(argv[1], "read") != 0) {
@@ -212,29 +187,33 @@ int main(int argc, char **argv) {
         return 2;
     }
 
-    manager = ASensorManager_getInstance();
-    if (manager == NULL) {
-        fprintf(stderr, "Android sensor manager is unavailable\n");
+    ALooper *looper = ALooper_prepare(ALOOPER_PREPARE_ALLOW_NON_CALLBACKS);
+    if (looper == NULL) {
+        fprintf(stderr, "Android event loop is unavailable\n");
         return 1;
     }
 
-    sensor = find_accelerometer(manager);
-    if (sensor == NULL) {
-        fprintf(stderr, "no default accelerometer is available\n");
+    struct android_accelerometer source;
+    enum android_accelerometer_status open_status =
+        android_accelerometer_open(&source, looper, sensor_looper_id, NULL);
+    if (open_status != ANDROID_ACCELEROMETER_OK) {
+        fprintf(stderr, "%s\n", android_accelerometer_status_text(open_status));
         return 1;
     }
 
+    int result;
     if (strcmp(path, accelerometer_info_path) == 0) {
-        return write_info(sensor);
-    }
-    if (strcmp(path, accelerometer_sample_path) == 0) {
-        return stream_accelerometer(manager, sensor, 0, 1);
-    }
-    if (strcmp(path, accelerometer_events_path) == 0) {
-        return stream_accelerometer(manager, sensor, 1, event_count);
+        result = write_info(&source);
+    } else if (strcmp(path, accelerometer_sample_path) == 0) {
+        result = stream_accelerometer(&source, 0, 1);
+    } else if (strcmp(path, accelerometer_events_path) == 0) {
+        result = stream_accelerometer(&source, 1, event_count);
+    } else {
+        fprintf(stderr, "unknown hardware path: %s\n", path);
+        print_usage(argv[0]);
+        result = 2;
     }
 
-    fprintf(stderr, "unknown hardware path: %s\n", path);
-    print_usage(argv[0]);
-    return 2;
+    android_accelerometer_close(&source);
+    return result;
 }
